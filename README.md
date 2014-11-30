c240d19578d9a754051cbb7c3950460099b3ffed
Matthias Bilger <matthias@bilger.info>
2014-11-30 09:05:02 +0100

    Initial version of feediron

---
+++ README.md
@@ -0,0 +1,179 @@
+ttrss_plugin-feediron
+=======================
+
+This is a plugin for Tiny Tiny RSS (tt-rss). It allows you to replace an article's contents by the contents of an element on the linked URL's page, i.e. create a "full feed".
+
+
+Installation
+------------
+
+Checkout the directory into your plugins folder like this (from tt-RSS root directory):
+
+```sh
+$ cd /var/www/ttrss
+$ git clone git://github.com/m42e/ttrss_plugin-feediron.git plugins/feediron
+```
+
+Then enable the plugin in preferences.
+
+
+Configuration
+-------------
+
+The configuration is done in JSON format. In the preferences, you'll find a new tab called *FeedIron*. Use the large field to enter/modify the configuration data and click the **Save** button to store it.
+You can enter an URL in the field below and click **Test** to get a preview what the result will be if the filter is applied to the url. *Note:* Save before you test :).
+
+A configuration looks like this:
+
+```json
+{
+
+"heise.de": {
+    "type": "xpath",
+    "xpath": "div[@class='meldung_wrapper']",
+    "force_charset": "utf-8"
+},
+"berlin.de/polizei": {
+    "type": "xpath",
+    "xpath": "div[@class='bacontent']"
+},
+"n24.de": {
+    "type": "xpath",
+    "xpath": "div[@class='news']"
+},
+"golem0Bde0C": {
+    "type": "xpath",
+    "xpath": "article",
+	"multipage": {
+		"xpath": "ol/li/a[contains(@id, 'atoc_')]",
+		"append": true
+	},
+    "reformat": [
+        {
+            "type": "regex",
+            "pattern": "/(?:[a-z0-9A-Z\\/.\\:]*?)golem0Bde0C(.*)0Erss0Bhtml\\/story01.htm/",
+            "replace": "http://www.golem.de/$1.html"
+        },
+        {
+            "type": "replace",
+            "search": [
+                "0A",
+                "0C",
+                "0B",
+                "0E"
+            ],
+            "replace": [
+                "0",
+                "/",
+                ".",
+                "-"
+            ]
+        }
+    ]
+},
+"oatmeal": {
+    "type": "xpath",
+    "xpath": "div[@id='comic']"
+},
+"blog.beetlebum.de": {
+    "type": "xpath",
+    "xpath": "div[@class='entry-content']",
+    "cleanup": [ "header", "footer" ],
+},
+"www.spiegel.de": {
+    "type": "split",
+    "steps": [
+        {
+            "after": "/article-section clearfix\"\\W*>/",
+            "before": "/<div\\W*class=\"module-box home-link-box/"
+        },
+        {
+            "before": "/<div\\W*class=\"btwBarInArticles/"
+        }
+    ],
+    "cleanup" : [ "~<script([^<]|<(?!/script))*</script>~msi" ],
+    "force_unicode": true
+}
+
+}
+```
+
+The *array key* is part of the URL of the article links(!). You'll notice the `golem0Bde0C` in the last entry: That's because all their articles link to something like `http://rss.feedsportal.com/c/33374/f/578068/p/1/s/3f6db44e/l/0L0Sgolem0Bde0Cnews0Cthis0Eis0Ean0Eexample0A10Erss0Bhtml/story01.htm` and to have the plugin match that URL and not interfere with other feeds using *feedsportal.com*, I used the part `golem0Bde0C`.
+
+**type** has to be `xpath` or `split`.
+
+### xpath
+The **xpath** value is the actual Xpath-element to fetch from the linked page. Omit the leading `//` - they will get prepended automatically.
+
+There is an additional option **cleanup** available. Its an array of Xpath-elements (relative to the fetched node) to remove from the fetched node. Omit the leading `//` - they will get prepended automatically.
+
+### split
+The **steps** value is an array of actions performed in the given order. If **after** is given the content will be split using the value and the second half is used, if **before** the first half is used. preg_split is used for this action.
+
+There is an additional option **cleanup** available. Its an array of regex that are removed using preg_replace.
+
+## multipage
+This option indicates that the article is split into two or more pages (eventually). FeedIron can combine all the parts into the content of the article.
+You have to specify a ```xpath``` which identifies the links (&lt;a&gt;) to the pages. If ```append``` is false, only the links are used and the original link is ignored else the links found using the xpath expression are added to the original page link.
+
+### General options
+**debug** You can activate debugging informations.  
+**force_charset** allows to override automatic charset detection. If it is omitted, the charset will be parsed from the HTTP headers or loadHTML() will decide on its own.  
+**reformat** is an array of formating rules for the **url** of the full article. The rules are applied before the full article is fetched. There are two possible types: **regex** and **replace**. **regex** takes a regex in an option called **pattern** and the replacement in **replace**. For details see [preg_replace](http://www.php.net/manual/de/function.preg-replace.php) in the PHP documentation. **replace** uses the PHP function str_replace, which takes either a string or an array as search and replace value.  
+**modify** is the same as described above but for the content. It is applied after the split/xpath selection.
+
+If you get an error about "Invalid JSON!", you can use [JSONLint](http://jsonlint.com/) to locate the erroneous part.
+
+XPath
+-----
+
+### Tools
+
+To test your XPath expressions, you can use these Chrome extensions:
+
+* [XPath Helper](https://chrome.google.com/webstore/detail/xpath-helper/hgimnogjllphhhkhlmebbmlgjoejdpjl)
+* [xPath Viewer](https://chrome.google.com/webstore/detail/xpath-viewer/oemacabgcknpcikelclomjajcdpbilpf)
+* [xpathOnClick](https://chrome.google.com/webstore/detail/xpathonclick/ikbfbhbdjpjnalaooidkdbgjknhghhbo)
+
+
+### Examples
+
+Some XPath expressions you could need (the `//` is automatically prepended and must be omitted in the FeedMod configuration):
+
+##### HTML5 &lt;article&gt; tag
+
+```html
+<article>…article…</article>
+```
+
+```xslt
+//article
+```
+
+##### DIV inside DIV
+
+```html
+<div id="content"><div class="box_content">…article…</div></div>`
+```
+
+```xslt
+//div[@id='content']/div[@class='box_content']
+```
+
+##### Multiple classes
+
+```html
+<div class="post-body entry-content xh-highlight">…article…</div>
+```
+
+```xslt
+//div[starts-with(@class ,'post-body')]
+```
+or
+```xslt
+//div[contains(@class, 'entry-content')]
+```
+
+## Special Thanks
+Thanks to mbirth who wrote af_feedmod, which gave me a starting base.
+
---
+++ init.php
@@ -0,0 +1,421 @@
+<?php
+
+class Af_Feediron extends Plugin implements IHandler
+{
+    private $host;
+    private $debug;
+    private $charset;
+
+    function about()
+    {
+        return array(
+            1.0,   // version
+            'Irons a feeds content to your needs',   // description
+            'm42e',   // author
+            false,   // is_system
+        );
+    }
+
+    function api_version()
+    {
+        return 2;
+    }
+
+    function init($host)
+    {
+        $this->host = $host;
+        $host->add_hook($host::HOOK_PREFS_TABS, $this);
+        $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+    }
+
+    function csrf_ignore($method)
+    {
+        $csrf_ignored = array("index", "edit");
+        return array_search($method, $csrf_ignored) !== false;
+    }
+
+    function before($method)
+    {
+        if ($_SESSION["uid"]) {
+            return true;
+        }
+        return false;
+    }
+
+    function after()
+    {
+        return true;
+    }
+
+    private function _log($msg) {
+       if ($this->debug) trigger_error($msg, E_USER_WARNING);
+    } 
+
+    function hook_article_filter($article)
+    {
+        if (($config = $this->getConfigSection($article['link'])) !== FALSE){
+            $articleMarker = "feediron:".$article['owner_uid'].",".md5(print_r($config, true)).":";
+            if (strpos($article['plugin_data'], $articleMarker) !== false) {
+                // do not process an article more than once
+                if (isset($article['stored']['content'])) $article['content'] = $article['stored']['content'];
+                return $article;
+            }
+            
+            $link = $this->reformatUrl($article['link'], $config);
+                     
+            $article['content'] = $this->getNewContent($link, $config);
+            $article['plugin_data'] = $articleMarker . $article['plugin_data'];                        
+        }
+
+        return $article;
+    }
+
+    function getConfigSection($url){
+        $data = $this->getConfig(); 
+        if(is_array($data)){
+           foreach ($data as $urlpart=>$config) {
+              if (strpos($url, $urlpart) === false) continue;   // skip this config if URL not matching
+              return $config;
+           }
+        }
+        return FALSE;
+    }
+
+    function getConfig(){
+       $json_conf = $this->host->get($this, 'json_conf');
+       $data = json_decode($json_conf, true);
+       $this->debug = isset($data['debug']) && $data['debug'];
+       return $data;
+    }
+
+    function reformatUrl($url, $config){
+       $link = trim($url);
+       if(is_array($config['reformat'])){
+          $link = $this->reformat($link, $config['reformat']);
+       }
+       return $link;
+    }
+    
+    function reformat($string, $options){
+        foreach($options as $option){
+            switch($option['type']){
+                case 'replace':
+                    $string = str_replace($option['search'], $option['replace'], $string);
+                break;
+                case 'regex':
+                    $string = preg_replace($option['pattern'], $option['replace'], $string);
+                break;
+            }
+        }
+        return $string;
+    }
+
+    function getNewContent($link, $config){
+		if (isset($config['multipage'])){
+			$links = $this->fetch_links($link, $config);
+		}else{
+			$links = array($link);
+		}
+		$html_complete = "";
+		foreach($links as $lnk){
+		   $html = $this->getArticleContent($lnk, $config);
+		   $html = $this->processArticle($html, $config);
+		   $html_complete .= $html;
+		}
+		return $html_complete;
+
+    }
+    function getArticleContent($link, $config){
+		list($html, $content_type) = $this->get_content($link);
+
+       $this->charset = false;
+       if (!isset($config['force_charset'])) {
+          if ($content_type) {
+             preg_match('/charset=(\S+)/', $content_type, $matches);
+             if (isset($matches[1]) && !empty($matches[1])) $this->charset = $matches[1];
+          }
+       } else {
+          // use forced charset
+          $this->charset = $config['force_charset'];
+       }
+
+       if ($this->charset && isset($config['force_unicode']) && $config['force_unicode']) {
+          $html = iconv($this->charset, 'utf-8', $html);
+          $this->charset = 'utf-8';
+       }
+       return $html;
+    }
+	function get_content($link){
+        global $fetch_last_content_type;
+		  $this->_log($link);
+       if (version_compare(VERSION, '1.7.9', '>=')) {
+          $html = fetch_file_contents($link);
+          $content_type = $fetch_last_content_type;
+       } else {
+          // fallback to file_get_contents()
+          $html = file_get_contents($link);
+
+          // try to fetch charset from HTTP headers
+          $headers = $http_response_header;
+          $content_type = false;
+          foreach ($headers as $h) {
+             if (substr(strtolower($h), 0, 13) == 'content-type:') {
+                $content_type = substr($h, 14);
+                // don't break here to find LATEST (if redirected) entry
+             }
+          }
+       }
+		return array( $html,  $content_type);
+	}
+	function fetch_links($link, $config){
+		$html = $this->getArticleContent($link, $config);
+		$links = $this->extractlinks($html, $config);
+		if (count($links) == 0)
+			return array($link);
+		$links = $this->fixlinks($link, $links);
+		foreach ($links as $lnk){
+			 $this->_log("link:".$lnk);
+		}
+		if(isset($config['multipage']['append']) && $config['multipage']['append']){
+			array_unshift($links, $link);
+		}
+		return $links;
+
+	}
+	function extractlinks($html, $config){
+       $doc = $this->getDOM($html);
+	   $links = array();
+
+	   $xpath = new DOMXPath($doc);
+	   $entries = $xpath->query('(//'.$config['multipage']['xpath'].')');   // find main DIV according to config
+
+
+	   if ($entries->length < 1) return array();
+	   foreach($entries as $entry){
+		   $links[] = $entry->getAttribute('href');
+	   }
+	   return $links;
+	}
+	function getDOM($html){
+       $doc = new DOMDocument(); 
+       if ($this->charset) {
+          $html = '<?xml encoding="' . $this->charset . '">' . $html;
+       }
+	   libxml_use_internal_errors(true);
+       $doc->loadHTML($html);
+	   if(!$doc){
+		   $this->_log("The content is not a valid xml format");
+		   if($this->debug){
+			   foreach (libxml_get_errors() as $value) {
+				   $this->_log($value);
+			   }
+		   }
+		   return new DOMDocument();
+	   }
+	   return $doc;
+	}
+	function fixlinks($link, $links){
+		$retlinks = array();
+		foreach($links as $lnk){
+			$retlinks[] = $this->rel2abs($lnk, $link);
+		}
+		return $retlinks;
+	}
+	function rel2abs($rel, $base)
+	{
+		if (parse_url($rel, PHP_URL_SCHEME) != '' || substr($rel, 0, 2) == '//') return $rel;
+		if ($rel[0]=='#' || $rel[0]=='?') return $base.$rel;
+		extract(parse_url($base));
+		$path = preg_replace('#/[^/]*$#', '', $path);
+		if ($rel[0] == '/') $path = '';
+
+		/* dirty absolute URL */
+		$abs = "$host$path/$rel";
+
+		/* replace '//' or '/./' or '/foo/../' with '/' */
+		$re = array('#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#');
+		for($n=1; $n>0; $abs=preg_replace($re, '/', $abs, -1, $n)) {}
+
+		/* absolute URL is ready! */
+		return $scheme.'://'.$abs;
+	}
+    function processArticle($html, $config){
+       switch ($config['type']) {
+       case 'split':
+          $html = $this->performSplit($html, $config);
+          break;
+
+       case 'xpath':
+          $html = $this->performXpath($html, $config);
+          break;
+
+       default:
+          continue;
+       }
+       if(is_array($config['modify'])){
+          $html = $this->reformat($html, $config['modify']);
+       }
+       return $html;
+    }
+
+    function performSplit($html, $config){
+         $orig_html = $html;
+          foreach($config['steps'] as $step){
+             if(isset($step['after'])){
+                $result = preg_split ($step['after'], $html);
+                $html = $result[1];
+             }
+             if(isset($step['before'])){
+                $result = preg_split ($step['before'], $html);
+                $html = $result[0];
+             }
+          }
+          if(strlen($html) == 0)
+             return $orig_html;
+          if(isset($config['cleanup'])){
+             foreach($config['cleanup'] as $cleanup){
+                $html = preg_replace($cleanup, '', $html);
+             }
+          }
+          return $html;
+    }
+
+    function performXpath($html, $config){
+		$doc = $this->getDOM($html);
+		$basenode = false;
+		$xpath = new DOMXPath($doc);
+		$entries = $xpath->query('(//'.$config['xpath'].')');   // find main DIV according to config
+
+		if ($entries->length > 0) $basenode = $entries->item(0);
+
+		if (!$basenode) return $html;
+
+		// remove nodes from cleanup configuration
+		$basenode = $this->cleanupNode($xpath, $basenode, $config);
+		$html = $doc->saveXML($basenode);
+		return $html;
+    }
+
+    function cleanupNode($xpath, $basenode, $config){
+       if(($cconfig = $this->getCleanupConfig($config))!== FALSE){
+          foreach ($cconfig as $cleanup) {
+             if(strpos($cleanup, "./") !== 0){
+                $cleanup = '//'.$cleanup;
+             }
+             $nodelist = $xpath->query($cleanup, $basenode);
+             foreach ($nodelist as $node) {
+                if ($node instanceof DOMAttr) {
+                   $node->ownerElement->removeAttributeNode($node);
+                }
+                else {
+                   $node->parentNode->removeChild($node);
+                }
+             }
+          }
+       }
+       return $basenode;
+    }
+
+    function getCleanupConfig($config){
+       $cconfig = false;
+       
+       if (isset($config['cleanup'])) {
+          $cconfig = $config['cleanup'];
+          if (!is_array($cconfig)) {
+             $cconfig = array($cconfig);
+          }
+       }
+       return $cconfig;
+    }
+
+    function hook_prefs_tabs($args)
+    {
+        print '<div id="feedironConfigTab" dojoType="dijit.layout.ContentPane"
+            href="backend.php?op=af_feediron"
+            title="' . __('FeedIron') . '"></div>';
+    }
+
+    function index()
+    {
+        $pluginhost = PluginHost::getInstance();
+        $json_conf = $pluginhost->get($this, 'json_conf');
+
+        print "<form dojoType=\"dijit.form.Form\">";
+
+        print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+            evt.preventDefault();
+            if (this.validate()) {
+                new Ajax.Request('backend.php', {
+                    parameters: dojo.objectToQuery(this.getValues()),
+                    onComplete: function(transport) {
+                        if (transport.responseText.indexOf('error')>=0) notify_error(transport.responseText);
+                            else notify_info(transport.responseText);
+                    }
+                });
+            }
+            </script>";
+
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"af_feediron\">";
+
+        print "<table width='100%'><tr><td>";
+        print "<textarea dojoType=\"dijit.form.SimpleTextarea\" name=\"json_conf\" style=\"font-size: 12px; width: 99%; height: 500px;\">$json_conf</textarea>";
+        print "</td></tr></table>";
+
+        print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
+
+        print "</form>";
+        print "<form dojoType=\"dijit.form.Form\">";
+
+        print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+            evt.preventDefault();
+            dojo.query('#test_result').attr('innerHTML', '');
+            new Ajax.Request('backend.php', {
+                parameters: dojo.objectToQuery(this.getValues()),
+                onComplete: function(transport) {
+                    if (transport.responseText.indexOf('error')>=0 && transport.responseText.indexOf('error') <= 10) notify_error(transport.responseText);
+                    else
+                       dojo.query('#test_result').attr('innerHTML', transport.responseText);
+                }
+            });
+            </script>";
+
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"test\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"af_feediron\">";
+
+        print "<table width='100%'><tr><td>";
+        print "<input dojoType=\"dijit.form.TextBox\" name=\"test_url\" style=\"font-size: 12px; width: 99%;\" />";
+        print "</td></tr></table>";
+        print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Test")."</button>";
+        print "</form>";
+        print "<div id='test_result'></div>";
+    }
+
+    function save()
+    {
+        $json_conf = $_POST['json_conf'];
+
+        if (is_null(json_decode($json_conf))) {
+            echo __("error: Invalid JSON!");
+            return false;
+        }
+
+        $this->host->set($this, 'json_conf', $json_conf);
+        echo __("Configuration saved.");
+    }
+
+    function test(){
+       $test_url = $_POST['test_url'];
+       $config = $this->getConfigSection($test_url);
+       $test_url = $this->reformatUrl($test_url, $config);
+       if($config === FALSE) 
+          echo "error: URL did not match";
+       else
+       {
+          echo "<h1>RESULT:</h1>";
+          echo $this->getNewContent($test_url, $config); 
+       }
+    }
+}
