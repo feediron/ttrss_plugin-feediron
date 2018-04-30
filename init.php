01541af8b439f65467a9eee2ada20841f8e3d08d
dugite-code <dugite-code@peekread.info>
2018-04-30 10:32:01 +0800

    Catch base html return

--- init.php
+++ init.php
@@ -281,7 +281,12 @@ class Feediron extends Plugin implements IHandler
 	{
 		$tagpath['xpath'] = $xpath;
 		Feediron_Logger::get()->log_html(Feediron_Logger::LOG_TTRSS, "Tag xpath: $xpath");
-		$tags[$key] .= trim( preg_replace('/\s+/', ' ', strip_tags( $this->performXpath( $html, $tagpath ) ) ) );
+		$rawtag = $this->performXpath( $html, $tagpath );
+		if( $rawtag != $html ){
+			Feediron_Logger::get()->log_html(Feediron_Logger::LOG_TTRSS, "No Tag found");
+			continue;
+		}
+		$tags[$key] .= trim( preg_replace('/\s+/', ' ', strip_tags( $rawtag ) ) );
 		Feediron_Logger::get()->log_html(Feediron_Logger::LOG_TTRSS, "Tag found: $tags[$key]");
 	}
 
