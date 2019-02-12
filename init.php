b782e806a706e57b4bd5bf8b3c708a9fbae5b958
dugite-code <dugite-code@peekread.info>
2019-02-12 16:42:14 +0800

    Added tag regex and search functions

--- init.php
+++ init.php
@@ -297,6 +297,14 @@ class Feediron extends Plugin implements IHandler
         $tags = $this->perform_tag_xpath($html, $config);
         break;
 
+      case 'regex':
+        $tags = $this->perform_tag_regex($html, $config);
+        break;
+
+      case 'search':
+        $tags = $this->perform_tag_search($html, $config);
+        break;
+
       default:
         Feediron_Logger::get()->log(Feediron_Logger::LOG_TTRSS, "Unrecognized option: ".$config['type']);
         break;
@@ -329,6 +337,55 @@ class Feediron extends Plugin implements IHandler
     return $tags;
   }
 
+  function perform_tag_regex($html, $config)
+  {
+    if(!is_array($config['pattern'])){
+      $patterns = array($config['pattern']);
+    }else{
+      $patterns = $config['pattern'];
+    }
+
+    // loop through regex pattern array
+    foreach( $patterns as $key=>$pattern ){
+      preg_match($pattern, $html, $tags[$key]);
+    }
+    return $tags;
+  }
+
+  function perform_tag_search($html, $config)
+  {
+    if(!is_array($config['pattern'])){
+      $patterns = array($config['pattern']);
+    }else{
+      $patterns = $config['pattern'];
+    }
+
+    if(!is_array($config['match'])){
+      $matches = array($config['match']);
+    }else{
+      $matches = $config['match'];
+    }
+
+    if( count($patterns) != count($matches) ){
+      Feediron_Logger::get()->log(Feediron_Logger::LOG_TTRSS, "Number of Patterns ".count($patterns)." doesn't equal number of Matches ".count($matches);
+      return;
+    }
+
+    $matches = array_combine ( $patterns, $matches );
+
+    // loop through regex pattern array
+    foreach( $matches as $pattern=>$match ){
+      if( preg_match($pattern, $html) && substr( $match, 0, 1 ) != "!" ){
+        Feediron_Logger::get()->log(Feediron_Logger::LOG_VERBOSE, "Tag search match", $pattern);
+        $tags[$pattern] .= $match;
+      } else if(substr( $match, 0, 1 ) == "!") {
+        Feediron_Logger::get()->log(Feediron_Logger::LOG_VERBOSE, "Tag inverted search match", $pattern);
+        $tags[$pattern] .= substr( $match, 1 );
+      }
+    }
+    return array_values( $tags );
+  }
+
   function perform_tag_xpath($html, $config)
   {
     if(!is_array($config['xpath'])){
@@ -341,7 +398,7 @@ class Feediron extends Plugin implements IHandler
     foreach( $xpaths as $key=>$xpath )
     {
       // set xpath in config
-      Feediron_Logger::get()->log(Feediron_Logger::LOG_VERBOSE, "Tag xpath: ", $xpath);
+      Feediron_Logger::get()->log(Feediron_Logger::LOG_VERBOSE, "Tag xpath", $xpath);
       $newtag = $this->performXpath( $html, $config );
 
       // Filter bad tags
