10ae06802319c7884a96957551b1209ab94890d0
dugite-code <dugite-code@peekread.info>
2019-03-08 10:46:31 +0800

    Version Bump

--- init.php
+++ init.php
@@ -25,7 +25,7 @@ class Feediron extends Plugin implements IHandler
   function about()
   {
     return array(
-      1.20,   // version
+      1.2.1,   // version
       'Reforge your feeds',   // description
       'm42e',   // author
       false,   // is_system
