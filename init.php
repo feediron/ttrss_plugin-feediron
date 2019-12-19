49f67a092ed976d3f8d39ca88dc0614e0dfba509
kuehnelth <kuehnelth@gmail.com>
2019-12-19 23:16:48 +0100

    Fix PHP deprecated warning (#144)
    "Array and string offset access syntax with curly braces is deprecated"

--- init.php
+++ init.php
@@ -480,7 +480,7 @@ class Feediron extends Plugin implements IHandler
     // Step 3
     if (preg_match('!^[a-z]+:!i', $url)) return $url;
     $base = parse_url($base);
-    if ($url{0} == "#") {
+    if ($url[0] == "#") {
       // Step 2 (fragment)
       $base['fragment'] = substr($url, 1);
       return $this->unparse_url($base);
@@ -493,7 +493,7 @@ class Feediron extends Plugin implements IHandler
         'scheme'=>$base['scheme'],
         'path'=>substr($url,2),
       ));
-    } else if ($url{0} == "/") {
+    } else if ($url[0] == "/") {
       // Step 5
       $base['path'] = $url;
     } else {
