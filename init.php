ad7f4948212c5d39749a18de159d2044f7a48335
dugite-code <dugite-code@users.noreply.github.com>
2023-05-24 14:31:34 +0800

    multipage fix
    On multipage parsing nested seenlinks arrays caused errors

--- init.php
+++ init.php
@@ -460,7 +460,8 @@ class Feediron extends Plugin implements IHandler
       /* If recursive mode is active fetch links from newly fetched link */
       if(isset($config['multipage']['recursive']) && $config['multipage']['recursive'] && !($counter == ($maxpages-1)) )
       {
-        $links =  $this->fetch_links($lnk, $config, $counter, $maxpages, array($links, $link));
+        array_push($links, $link); // Add $link to $links array
+        $links =  $this->fetch_links($lnk, $config, $counter, $maxpages, $links);
       }
     }
     if(isset($config['multipage']['append']) && $config['multipage']['append'])
